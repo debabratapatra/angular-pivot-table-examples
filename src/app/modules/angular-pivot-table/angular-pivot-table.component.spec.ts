5e7a8f5b6c841f96652b139c141bbcc32d71fcfe
debabratapatra <debabratapatra12@gmail.com>
2022-06-13 10:49:08 +0530

    Upgrade to 11.

--- src/app/modules/angular-pivot-table/angular-pivot-table.component.spec.ts
+++ src/app/modules/angular-pivot-table/angular-pivot-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AngularPivotTableComponent } from './angular-pivot-table.component';
 
@@ -6,7 +6,7 @@ describe('AngularPivotTableComponent', () => {
   let component: AngularPivotTableComponent;
   let fixture: ComponentFixture<AngularPivotTableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AngularPivotTableComponent ]
     })
